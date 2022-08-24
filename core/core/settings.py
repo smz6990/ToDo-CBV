12b861787adde0579c0296a29d136d04180e11f4
saleh <saleh.mohammadzadeh@gmail.com>
2022-08-25 01:57:17 +0430

    adding docs to api
    add swagger and redoc for api documentation with dsf-yasg module , adding their urls to core url , updating requirements file .

--- core/core/settings.py
+++ core/core/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     
     'rest_framework',
     'django_filters',
+    'drf_yasg',
     
 ]
 
--- core/core/urls.py
+++ core/core/urls.py
@@ -20,7 +20,25 @@ from django.conf import settings
 from django.conf.urls.static import static
 from accounts import views
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 from rest_framework.documentation import include_docs_urls
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="ToDo API",
+      default_version='v1',
+      description="ToDo API documentation",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="saleh.mohammadzadeh@gmail.com"),
+      license=openapi.License(name="MIT License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,6 +47,11 @@ urlpatterns = [
     path('signup',views.SignupCreateView.as_view(),name='signup'),
     path('api-auth/', include('rest_framework.urls')),
     path('api-docs/', include_docs_urls(title='API Documentation')),
+
+    path('swagger.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger.yaml', schema_view.without_ui(cache_timeout=0), name='schema-yaml'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
 ]
 
