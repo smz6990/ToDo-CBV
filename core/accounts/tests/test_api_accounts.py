be464295ce565cc6e64c363500f4e33490d183fd
saleh <saleh.mohammadzadeh@gmail.com>
2022-09-05 19:40:29 +0430

    create new fixture for create user object and not verified user and create author and task object

--- core/accounts/tests/test_api_accounts.py
+++ core/accounts/tests/test_api_accounts.py
@@ -7,243 +7,245 @@ from accounts.models import User
 
 @pytest.fixture
 def api_client():
-    return  APIClient
+    client = APIClient()
+    return  client
+
+@pytest.fixture
+def create_user_obj():
+    user = User.objects.create_user(email='test@test.com', password='a/1234567', is_verified=True)
+    return user
+
+@pytest.fixture
+def not_verified_user(create_user_obj):
+    user = create_user_obj
+    user.is_verified = False
+    user.save()
+    return user
 
 @pytest.mark.django_db
-class TestAccountsApi():
+class TestAccountsApi:
     
     endpoint = 'accounts:api-v1'
     
-    def create_user_obj(self,is_verified=True):
-        return User.objects.create_user(email='test@test.com', password='a/1234567', is_verified=is_verified)
-    
-    
-    
     def test_registration_post_201(self, api_client):
-        client = api_client()
         registration_endpoint = reverse( f'{self.endpoint}:registration' )
         data = {'email':'test@test.com', 'password':'a/1234567', 'password1':'a/1234567'}
-        response = client.post(path=registration_endpoint, data=data)
+        response = api_client.post(path=registration_endpoint, data=data)
         assert response.status_code == 201
     
-    def test_registration_post_400_existing_email(self, api_client):
+    def test_registration_post_400_existing_email(self, api_client, create_user_obj):
         # with existing email
-        client = api_client()
-        user = self.create_user_obj()
+        user = create_user_obj
         registration_endpoint = reverse( f'{self.endpoint}:registration' )
         data = {'email':'test@test.com', 'password':'a/1234567', 'password1':'a/1234567'}
-        response = client.post(path=registration_endpoint, data=data)
+        response = api_client.post(path=registration_endpoint, data=data)
         assert response.status_code == 400
     
     def test_registration_post_400_mismatch_password(self, api_client):
         # mismatch password
-        client = api_client()
         registration_endpoint = reverse( f'{self.endpoint}:registration' )
         data = {'email':'test@test.com', 'password':'a/1234567', 'password1':'a/7654321'}
-        response = client.post(path=registration_endpoint, data=data)
+        response = api_client.post(path=registration_endpoint, data=data)
         assert response.status_code == 400
         
-    def test_token_login_post_200(self, api_client):
-        client = api_client()
+    def test_token_login_post_200(self, api_client, create_user_obj):
         login_endpoint = reverse( f'{self.endpoint}:token-login' )
-        user = self.create_user_obj()
+        user = create_user_obj
         data = {'email':user.email, 'password':'a/1234567'}
-        response = client.post(path=login_endpoint, data=data)
+        response = api_client.post(path=login_endpoint, data=data)
         assert response.status_code == 200
 
-    def test_token_login_post_403_logged_user(self, api_client):
-        client = api_client()
+    def test_token_login_post_403_logged_user(self, api_client, create_user_obj):
         login_endpoint = reverse( f'{self.endpoint}:token-login' )
-        user = self.create_user_obj()
-        client.force_authenticate(user)
+        user = create_user_obj
+        api_client.force_authenticate(user)
         data = {'email':user.email, 'password':'a/1234567'}
-        response = client.post(path=login_endpoint, data=data)
+        response = api_client.post(path=login_endpoint, data=data)
         assert response.status_code == 403
        
-    def test_token_logout_post_204(self, api_client):
-        client = api_client()
+    def test_token_logout_post_204(self, api_client, create_user_obj):
         logout_endpoint = reverse( f'{self.endpoint}:token-logout' )
-        user = self.create_user_obj()
-        client.force_authenticate(user)
-        response = client.post(path=logout_endpoint, data=None)
+        user = create_user_obj
+        api_client.force_authenticate(user)
+        response = api_client.post(path=logout_endpoint, data=None)
         assert response.status_code == 204
     
     def test_token_logout_post_not_logged_user(self, api_client):
-        client = api_client()
         logout_endpoint = reverse( f'{self.endpoint}:token-logout' )
-        response = client.post(path=logout_endpoint, data=None)
+        response = api_client.post(path=logout_endpoint, data=None)
         assert response.status_code == 401
     
-    def test_change_password_204(self, api_client):
-        client = api_client()
+    def test_change_password_204(self, api_client, create_user_obj):
         change_password_endpoint = reverse( f'{self.endpoint}:change-password' )
-        user = self.create_user_obj()
-        client.force_authenticate(user)
+        user = create_user_obj
+        api_client.force_authenticate(user)
         data = {'old_password':'a/1234567', 'new_password':'a/7654321', 'new_password1':'a/7654321'}
-        response = client.put(path=change_password_endpoint, data=data)
+        response = api_client.put(path=change_password_endpoint, data=data)
         assert response.status_code == 204
     
-    def test_change_password_400_wrong_password(self, api_client):
+    def test_change_password_400_wrong_password(self, api_client, create_user_obj):
         # wrong old password
-        client = api_client()
         change_password_endpoint = reverse( f'{self.endpoint}:change-password' )
-        user = self.create_user_obj()
-        client.force_authenticate(user)
+        user = create_user_obj
+        api_client.force_authenticate(user)
         data = {'old_password':'a/7654321', 'new_password':'a/7654321', 'new_password1':'a/7654321'}
-        response = client.put(path=change_password_endpoint, data=data)
+        response = api_client.put(path=change_password_endpoint, data=data)
         assert response.status_code == 400
     
-    def test_change_password_400_mismatch_password(self, api_client):
+    def test_change_password_400_mismatch_password(self, api_client, create_user_obj):
         # mismatch new password
-        client = api_client()
         change_password_endpoint = reverse( f'{self.endpoint}:change-password' )
-        user = self.create_user_obj()
-        client.force_authenticate(user)
+        user = create_user_obj
+        api_client.force_authenticate(user)
         data = {'old_password':'a/1234567', 'new_password':'a/765765', 'new_password1':'a/7654321'}
-        response = client.put(path=change_password_endpoint, data=data)
+        response = api_client.put(path=change_password_endpoint, data=data)
         assert response.status_code == 400
     
-    def test_change_password_401_unauthorized_user(self, api_client):
+    def test_change_password_401_unauthorized_user(self, api_client, create_user_obj):
         # Unauthorized  user
-        client = api_client()
         change_password_endpoint = reverse( f'{self.endpoint}:change-password' )
-        user = self.create_user_obj()
+        user = create_user_obj
         data = {'old_password':'a/1234567', 'new_password':'a/7654321', 'new_password1':'a/7654321'}
-        response = client.put(path=change_password_endpoint, data=data)
+        response = api_client.put(path=change_password_endpoint, data=data)
         assert response.status_code == 401
     
-    def test_send_verification_email_200(self, api_client):
-        client = api_client()
-        user = self.create_user_obj(is_verified=False)
+    def test_send_verification_email_200(self, api_client, not_verified_user):
+        user = not_verified_user
         token = AccessToken.for_user(user)
-        client.force_authenticate(user)
+        api_client.force_authenticate(user)
         send_email_endpoint =  f'/accounts/api/v1/verification/confirm/{token}/'
-        response = client.get(path=send_email_endpoint)
+        response = api_client.get(path=send_email_endpoint)
         assert response.status_code == 200
     
-    def test_send_verification_email_400_invalid_token(self, api_client):
-        client = api_client()
-        user = self.create_user_obj(is_verified=False)
+    def test_send_verification_email_400_invalid_token(self, api_client, not_verified_user):
+        user = not_verified_user
         token = AccessToken.for_user(user)
-        client.force_authenticate(user)
+        api_client.force_authenticate(user)
         token = f'{str(token)}123'
         send_email_endpoint = f'/accounts/api/v1/verification/confirm/{token}/'
-        response = client.get(path=send_email_endpoint)
+        response = api_client.get(path=send_email_endpoint)
         assert response.status_code == 400
        
-    def test_resend_email_verification_200(self, api_client):
-        client = api_client()
+    def test_resend_email_verification_200(self, api_client, not_verified_user):
         resend_email_endpoint = reverse( f'{self.endpoint}:email-resend' )
-        user = self.create_user_obj(is_verified=False)
+        user = not_verified_user
         data = {'email':user.email}
-        response = client.post(path=resend_email_endpoint, data=data)
+        response = api_client.post(path=resend_email_endpoint, data=data)
         assert response.status_code == 200 
     
-    def test_resend_email_verification_400_verified_user(self, api_client):
+    def test_resend_email_verification_400_verified_user(self, api_client, create_user_obj):
         # verified user
-        client = api_client()
         resend_email_endpoint = reverse( f'{self.endpoint}:email-resend' )
-        user = self.create_user_obj(is_verified=True)
+        user = create_user_obj
         data = {'email':user.email}
-        response = client.post(path=resend_email_endpoint, data=data)
+        response = api_client.post(path=resend_email_endpoint, data=data)
         assert response.status_code == 400
         
     def test_resend_email_verification_400_no_user(self, api_client):
         # test with no user with this email (dont exist)
-        client = api_client()
         resend_email_endpoint = reverse( f'{self.endpoint}:email-resend' )
         data = {'email':'test@test.com'}
-        response = client.post(path=resend_email_endpoint, data=data)
+        response = api_client.post(path=resend_email_endpoint, data=data)
         assert response.status_code == 400
     
-    def test_password_reset_send_email_200(self, api_client):
-        client = api_client()
+    def test_password_reset_send_email_200(self, api_client, create_user_obj):
         password_reset_endpoint = reverse( f'{self.endpoint}:password-reset' )
-        user = self.create_user_obj(is_verified=True)
+        user = create_user_obj
         data = {'email':user.email}
-        response = client.post(path=password_reset_endpoint, data=data)
+        response = api_client.post(path=password_reset_endpoint, data=data)
         assert response.status_code == 200 
     
-    def test_password_reset_send_email_401_not_verified_user(self, api_client):
+    def test_password_reset_send_email_401_not_verified_user(self, api_client, not_verified_user):
         # user is not verified
-        client = api_client()
         password_reset_endpoint = reverse( f'{self.endpoint}:password-reset' )
-        user = self.create_user_obj(is_verified=False)
+        user = not_verified_user
         data = {'email':user.email}
-        response = client.post(path=password_reset_endpoint, data=data)
+        response = api_client.post(path=password_reset_endpoint, data=data)
         assert response.status_code == 401
     
     def test_password_reset_send_email_400_no_user(self, api_client):
         # user is not exist
-        client = api_client()
         password_reset_endpoint = reverse( f'{self.endpoint}:password-reset' )
         data = {'email':'test@test.com'}
-        response = client.post(path=password_reset_endpoint, data=data)
+        response = api_client.post(path=password_reset_endpoint, data=data)
         assert response.status_code == 400
     
-    def test_done_password_reset_200(self, api_client):
-        client = api_client()
-        user = self.create_user_obj(is_verified=False)
+    def test_done_password_reset_200(self, api_client, not_verified_user):
+        user = not_verified_user
         token = AccessToken.for_user(user)
         password_reset_done_endpoint = f'/accounts/api/v1/password-reset/done/{token}/'
         data = {'new_password':'a/9876543', 'new_password1':'a/9876543'}
-        response = client.put(path=password_reset_done_endpoint, data=data)
+        response = api_client.put(path=password_reset_done_endpoint, data=data)
         assert response.status_code == 200
     
-    def test_done_password_reset_400_mismatch_password(self, api_client):
+    def test_done_password_reset_400_mismatch_password(self, api_client, not_verified_user):
         # mismatch password
-        client = api_client()
-        user = self.create_user_obj(is_verified=False)
+        user = not_verified_user
         token = AccessToken.for_user(user)
         password_reset_done_endpoint = f'/accounts/api/v1/password-reset/done/{token}/'
         data = {'new_password':'a/9876543', 'new_password1':'a/1234567'}
-        response = client.put(path=password_reset_done_endpoint, data=data)
+        response = api_client.put(path=password_reset_done_endpoint, data=data)
         assert response.status_code == 400
     
-    def test_jwt_token_create_200(self, api_client):
-        client = api_client()
+    def test_jwt_token_create_200(self, api_client, create_user_obj): 
         jwt_endpoint = reverse( f'{self.endpoint}:token-obtain-pair' )
-        user = self.create_user_obj()
-        # client.force_authenticate(user=user)
+        user = create_user_obj
         data = {'email':user.email, 'password':'a/1234567'}
-        response = client.post(path=jwt_endpoint, data=data)
+        response = api_client.post(path=jwt_endpoint, data=data)
         assert response.status_code == 200
     
-    def test_jwt_token_create_400_not_verified_user(self, api_client):
+    def test_jwt_token_create_400_not_verified_user(self, api_client, not_verified_user):
         # user not verified
-        client = api_client()
         jwt_endpoint = reverse( f'{self.endpoint}:token-obtain-pair' )
-        user = self.create_user_obj(is_verified=False)
-        # client.force_authenticate(user=user)
+        user = not_verified_user
         data = {'email':user.email, 'password':'a/1234567'}
-        response = client.post(path=jwt_endpoint, data=data)
+        response = api_client.post(path=jwt_endpoint, data=data)
         assert response.status_code == 400
     
     def test_jwt_token_create_401_not_existing_user(self, api_client):
         # user not exist
-        client = api_client()
         jwt_endpoint = reverse( f'{self.endpoint}:token-obtain-pair' )
         data = {'email':'test@test.com', 'password':'a/1234567'}
-        response = client.post(path=jwt_endpoint, data=data)
+        response = api_client.post(path=jwt_endpoint, data=data)
         assert response.status_code == 401
     
-    def test_jwt_token_refresh_200(self, api_client):
-        client = api_client()
+    def test_jwt_token_refresh_200(self, api_client, create_user_obj):
         jwt_endpoint = reverse( f'{self.endpoint}:token-refresh' )
-        user = self.create_user_obj()
-        # client.force_authenticate(user=user)
+        user = create_user_obj
         refresh = str(RefreshToken.for_user(user))
         data = {"refresh":refresh}
-        response = client.post(path=jwt_endpoint, data=data)
+        response = api_client.post(path=jwt_endpoint, data=data)
         assert response.status_code == 200
     
-    def test_jwt_token_refresh_401_invalid_token(self, api_client):
+    def test_jwt_token_refresh_401_invalid_token(self, api_client, create_user_obj):
         # corrupted token
-        client = api_client()
         jwt_endpoint = reverse( f'{self.endpoint}:token-refresh' )
-        user = self.create_user_obj()
-        # client.force_authenticate(user=user)
+        user = create_user_obj
         refresh = f'{str(RefreshToken.for_user(user))}123'
         data = {"refresh":refresh}
-        response = client.post(path=jwt_endpoint, data=data)
+        response = api_client.post(path=jwt_endpoint, data=data)
+        assert response.status_code == 401
+        
+    def test_jwt_token_verify_200_with_token_refresh(self, api_client, create_user_obj):
+        jwt_endpoint = reverse(f"{self.endpoint}:token-verify")
+        user = create_user_obj
+        token = f"{str(RefreshToken.for_user(user))}"
+        data = {"token": token}
+        response = api_client.post(path=jwt_endpoint, data=data)
+        assert response.status_code == 200
+
+    def test_jwt_token_verify_200_with_token_access(self, api_client, create_user_obj):
+        jwt_endpoint = reverse(f"{self.endpoint}:token-verify")
+        user = create_user_obj
+        token = f"{str(RefreshToken.for_user(user).access_token)}"
+        data = {"token": token}
+        response = api_client.post(path=jwt_endpoint, data=data)
+        assert response.status_code == 200
+
+    def test_jwt_token_verify_401_with_invalid_token(self, api_client, create_user_obj):
+        jwt_endpoint = reverse(f"{self.endpoint}:token-verify")
+        user = create_user_obj
+        token = f"{str(RefreshToken.for_user(user))}123"
+        data = {"token": token}
+        response = api_client.post(path=jwt_endpoint, data=data)
         assert response.status_code == 401
--- core/todo/tests/test_api_todo.py
+++ core/todo/tests/test_api_todo.py
@@ -7,80 +7,77 @@ from accounts.models import User
 
 @pytest.fixture
 def api_client():
-    return  APIClient
+    client = APIClient()
+    return  client
+
+@pytest.fixture
+def create_user_obj():
+    user = User.objects.create_user(email='test@test.test', password='a/1234567', is_verified=True)
+    return user
+
+@pytest.fixture
+def create_author_and_task_obj(create_user_obj):
+    author = create_user_obj
+    task = Task.objects.create(author=author, content='test content')
+    return author, task
 
 @pytest.mark.django_db
-class TestTodoApi():
+class TestTodoApi:
     
     endpoint = reverse('todo:api-v1:task-list')
     
-    def create_user_obj(self, num=1):
-        return User.objects.create_user(email=f'test{num}@test.com', password='a/1234567', is_verified=True)
-    
-    def create_author_and_task_obj(self):
-        author = self.create_user_obj()
-        task = Task.objects.create(author=author, content='content')
-        return author, task
-    
-    def test_api_todo_get_task_list_response_200(self, api_client):
-        client = api_client()
-        user = self.create_user_obj()
-        client.force_authenticate(user=user)
-        response = client.get(self.endpoint)
+    def test_api_todo_get_task_list_response_200(self, api_client, create_user_obj):
+        user = create_user_obj
+        api_client.force_authenticate(user=user)
+        response = api_client.get(self.endpoint)
         assert response.status_code == 200
         
-    def test_api_todo_get_task_list_response_401(self, api_client):
-        client = api_client()
-        response = client.get(self.endpoint)
+    def test_api_todo_get_task_list_response_401_unauthorize_user(self, api_client):
+        #unauthorize user
+        response = api_client.get(self.endpoint)
         assert response.status_code == 401
         
-    def test_api_todo_post_task_list_response_201(self, api_client):
-        client = api_client()
-        author, task = self.create_author_and_task_obj()
-        client.force_authenticate(user=author)
+    def test_api_todo_post_task_list_response_201(self, api_client, create_author_and_task_obj):
+        author, task = create_author_and_task_obj
+        api_client.force_authenticate(user=author)
         data = {"author":author.email, "content":task.content, "is_done":task.is_done}
-        response = client.post(path=self.endpoint, data=data)
+        response = api_client.post(path=self.endpoint, data=data)
         assert response.status_code == 201
         
-    def test_api_todo_get_task_retrieve_response_200(self, api_client):
-        client = api_client()
-        author, task = self.create_author_and_task_obj()
-        client.force_authenticate(user=author)
+    def test_api_todo_get_task_retrieve_response_200(self, api_client, create_author_and_task_obj):
+        author, task = create_author_and_task_obj
+        api_client.force_authenticate(user=author)
         task_url = f'{self.endpoint}{task.id}/'
-        response = client.get(task_url)
+        response = api_client.get(task_url)
         assert response.status_code == 200
         
-    def test_api_todo_get_task_retrieve_response_404(self, api_client):
-        client = api_client()
-        author, task = self.create_author_and_task_obj()
-        user = self.create_user_obj(2)
-        client.force_authenticate(user=user)
+    def test_api_todo_get_task_retrieve_response_404(self, api_client, create_user_obj, create_author_and_task_obj):
+        author, task = create_author_and_task_obj
+        user = User.objects.create_user(email='test1@test.test', password='a/1234567', is_verified=True)
+        api_client.force_authenticate(user=user)
         task_url = f'{self.endpoint}{task.id}/'
-        response = client.get(task_url)
+        response = api_client.get(task_url)
         assert response.status_code == 404
 
-    def test_api_todo_put_task_response_200(self, api_client):
-        client = api_client()
-        author, task = self.create_author_and_task_obj()
-        client.force_authenticate(user=author)
+    def test_api_todo_put_task_response_200(self, api_client, create_author_and_task_obj):
+        author, task = create_author_and_task_obj
+        api_client.force_authenticate(user=author)
         task_url = f'{self.endpoint}{task.id}/'
         data = {"author":author.email, "content":"put content", "is_done":True}
-        response = client.put(path=task_url, data=data)
+        response = api_client.put(path=task_url, data=data)
         assert response.status_code == 200
     
-    def test_api_todo_patch_task_response_200(self, api_client):
-        client = api_client()
-        author, task = self.create_author_and_task_obj()
-        client.force_authenticate(user=author)
+    def test_api_todo_patch_task_response_200(self, api_client, create_author_and_task_obj):
+        author, task = create_author_and_task_obj
+        api_client.force_authenticate(user=author)
         task_url = f'{self.endpoint}{task.id}/'
         data = {"author":author.email, "content":"patch content"}
-        response = client.patch(path=task_url, data=data)
+        response = api_client.patch(path=task_url, data=data)
         assert response.status_code == 200
 
-    def test_api_todo_delete_task_response_204(self, api_client):
-        client = api_client()
-        author, task = self.create_author_and_task_obj()
-        client.force_authenticate(user=author)
+    def test_api_todo_delete_task_response_204(self, api_client, create_author_and_task_obj):
+        author, task = create_author_and_task_obj
+        api_client.force_authenticate(user=author)
         task_url = f'{self.endpoint}{task.id}/'
-        response = client.delete(path=task_url)
+        response = api_client.delete(path=task_url)
         assert response.status_code == 204
